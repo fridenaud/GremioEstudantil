7e2754e87ade07835f846e3c52c178935b62be69
fridenaud <131001401+fridenaud@users.noreply.github.com>
2023-11-13 11:42:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-alert('ola mundo');
+function tocarSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+
+document.querySelector('.tecla_aplausos').onclick = tocarSomAplausos;
