a945d5c0fe063d20c2ee8b306f44b7de456c4f07
fridenaud <131001401+fridenaud@users.noreply.github.com>
2023-11-27 11:29:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,61 @@
 function tocarSomAplausos(){
     document.querySelector('#som_tecla_aplausos').play();
 }
-
 document.querySelector('.tecla_aplausos').onclick = tocarSomAplausos;
+
+function tocarSomvaia(){
+    document.querySelector('#som_tecla_vaia').play();
+}
+document.querySelector('.tecla_errado').onclick = tocarSomerrado;
+
+function tocarSomerrado(){
+    document.querySelector('#som_tecla_errado').play();
+}
+document.querySelector('.tecla_errado').onclick = tocarSomerrado;
+
+function tocarSomgrilo(){
+    document.querySelector('#som_tecla_grilo').play();
+}
+document.querySelector('.tecla_grilo').onclick = tocarSomgrilo;
+
+
+function tocarSomtrombeta(){
+    document.querySelector('#som_tecla_trombeta').play();
+}
+document.querySelector('.tecla_trombeta').onclick = tocarSomtrombeta;
+
+function tocarSomtdinheiro(){
+    document.querySelector('#som_tecla_dinheiro').play();
+}
+document.querySelector('.tecla_dinheiro').onclick = tocarSomdinheiro;
+
+function tocarSomvitoria(){
+    document.querySelector('#som_tecla_vitoria').play();
+}
+document.querySelector('.tecla_vitoria').onclick = tocarSomvitoria;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
